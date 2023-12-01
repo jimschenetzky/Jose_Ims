3465749a080554c56b35146d2ae0da6ae741886f
Josefina Imschenetzky <87721128+jimschenetzky@users.noreply.github.com>
2023-12-01 19:18:22 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -7,4 +7,5 @@
 window.addEventListener('DOMContentLoaded', event => {
     const listHoursArray = document.body.querySelectorAll('.list-hours li');
     listHoursArray[new Date().getDay()].classList.add(('today'));
+    
 })
